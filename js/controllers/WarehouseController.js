abfb3290dd037194ae2efd481e0cadef9683aee9
Atul Kapoor <atulkapoor@gmail.com>
2016-06-10 17:45:14 +0530

    New screens and bug fixes

---
+++ js/controllers/WarehouseController.js
@@ -0,0 +1,11 @@
+angular.module('TriliaApp').controller('WarehouseController', function($rootScope, $scope, $http, $timeout) {
+    $scope.setAuth(true);
+    $scope.$on('$viewContentLoaded', function() {   
+        //App.initAjax(); // initialize core components
+        //Layout.setSidebarMenuActiveLink('set', $('#sidebar_menu_link_profile')); // set profile link active in sidebar menu 
+    });
+
+    // set sidebar closed and body solid layout mode
+    $rootScope.settings.layout.pageBodySolid = true;
+    $rootScope.settings.layout.pageSidebarClosed = false;
+}); 
